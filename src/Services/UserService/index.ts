3cc0befef3e93c8cb18a7a3c4eee94ce2700f92f
Maol <maulana.zulkifar@oppo-aed.id>
2024-05-31 09:46:52 +0700

    chore: add UserSevices & User Interface

---
+++ src/Services/UserService/index.ts
@@ -0,0 +1,17 @@
+import {User} from "../../Interfaces/User.ts";
+
+
+const UserService = async (): Promise<User[]> => {
+    try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+        if (!response.ok) {
+            throw new Error('Failed to fetch users');
+        }
+        return await response.json();
+    } catch (error) {
+        console.error('Error fetching users:', error.message);
+        throw error;
+    }
+};
+
+export default UserService;
